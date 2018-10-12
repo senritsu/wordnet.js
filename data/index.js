5c7546fc2734d686bc96a7f54e3b57f859bf96dd
senritsu <teh.b4k4@gmail.com>
2018-10-12 19:02:29 +0200

    split noun into 2 files

--- data/index.js
+++ data/index.js
@@ -1,10 +1,11 @@
-import noun from './noun.js'
+import noun1 from './noun1.js'
+import noun2 from './noun2.js'
 import adjective from './adjective.js'
 import verb from './verb.js'
 import adverb from './adverb.js'
 
 export default {
-  noun,
+  noun: noun1.concat(noun2),
   adjective,
   verb,
   adverb
