a90dd8bdf9614e09a96e548a628f8edd22a2615b
senritsu <teh.b4k4@gmail.com>
2018-10-12 18:03:34 +0200

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -10,31 +10,9 @@ exports.load_or_unzip = function(callback) {
       adverb: require("./data/Adverb").data,
     }
   }
-
-  let unzip_data = function(cb) {
-    console.log("unzipping wordnet, may take a moment..")
-    let exec = require('child_process').exec;
-    let command = "unzip ./data.zip"
-    exec(command, {
-      env: {
-        "PATH": "/usr/local/bin:/usr/bin:/bin" //is this cross-platform?
-      }
-    }, function(error, stdout) {
-      console.log(stdout);
-      cb()
-    })
-  }
-
-  if (require('fs').existsSync(__dirname + "/data/Adjective.js")) {
-    let o = load_data()
-    callback(o)
-  } else {
-    unzip_data(function() {
-      let o = load_data()
-      callback(o)
-    })
-  }
-
+  
+  let o = load_data()
+  callback(o)
 }
 
 exports.unique = function(a) {
